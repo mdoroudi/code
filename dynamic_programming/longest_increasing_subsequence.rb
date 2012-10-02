e785249a6f5cfb9c729d6f89cae435d44580d99d
Mina Doroudi <mina.doroudi@gmail.com>
2012-10-02 00:56:01 -0700

    DD: Longest Increasing SubSequence: for a given sequence find one of the LISS of the sequence

---
+++ dynamic_programming/longest_increasing_subsequence.rb
@@ -0,0 +1,45 @@
+require 'debugger'
+# Given a sequence of numbers, find the 'longest subsequence' of numbers
+# which is non-decreasing. The subsequence does not have to be contiguous or uniqe
+# 
+# ex
+# 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15, …
+# the Longest increasing sub sequence is 
+# 0, 2, 6, 9, 13, 15
+# or
+# 0, 4, 6, 9, 11, 15
+# so it does not have to be unique or continguous
+#
+def longest_increasing_subsequence(sqnc)
+  length = sqnc.size
+  len = [1]*length       # length of longest ISS for upto character i
+  parents_arr = [nil]*length  # predecesor of longest ISS of the sqnc[i]
+  largest = 0            # the length of the longest ISS found so far
+ 
+  len[0] = 1             # the first element in the list can't have a longer ISS than itself
+
+  for index in (1...length)
+    j = index - 1
+    while j >= 0 do
+      if sqnc[j] <= sqnc[index] && len[j] >= len[index]
+        len[index] = len[j]+1
+        parents_arr[index] = j
+        largest = [largest, len[index]].max
+      end
+      j -= 1
+    end
+  end
+
+  index = len.find_index(largest)
+  res = []
+  res << sqnc[index]
+  while !parents_arr[index].nil? do 
+    index = parents_arr[index]
+    res << sqnc[index]
+  end
+  res.reverse 
+end
+
+sequence = [ 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+puts longest_increasing_subsequence(sequence)
+
