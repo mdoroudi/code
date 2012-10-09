b6ad5b83494be4a6a65df464aa7a8fd2730d95fd
Mina Doroudi <mina.doroudi@gmail.com>
2012-10-08 21:42:16 -0700

    z

--- random/total_coins_representation.rb
+++ random/total_coins_representation.rb
@@ -1,24 +1,26 @@
-
+require 'debugger'
 def total_coins_representation(n)
-  total_representation(n, 0, [])
+  puts "total ways to represent #{n}:"
+  puts total_representation(n, 0, [])
 end
 def total_representation(n, total_coins, representations)
-  if n == 0 
-    puts total_coins
-    representations << total_coins
-    return representations
-  end
-  return -1 if n < 0
+  return 1 if n == 0 
+  return 1 if n == 1
+  return 0 if n < 0
 
-  total_representation(n - 25, total_coins+1, representations) if n >= 25
-  total_representation(n - 5, total_coins+1, representations ) if n >= 5
-  total_representation(n - 10, total_coins+1, representations) if n >= 10
-  total_representation(n - 1, total_coins+1, representations) if n >= 1
+  ways = 
+    total_representation(n - 25, total_coins+1, representations) + 
+    total_representation(n - 5, total_coins+1, representations ) +
+    total_representation(n - 10, total_coins+1, representations) + 
+    total_representation(n - 1, total_coins+1, representations) 
+  return ways
 end
 
-#total_coins_representation(1)
-#total_coins_representation(2)
-#total_coins_representation(3)
-#total_coins_representation(4)
+total_coins_representation(1)
+total_coins_representation(2)
+total_coins_representation(3)
+total_coins_representation(4)
 total_coins_representation(5)
-#total_coins_representation(13)
+total_coins_representation(13)
+total_coins_representation(10)
+total_coins_representation(9)
