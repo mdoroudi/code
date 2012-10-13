b57eff2d2666ba7162591579c9a7e63068a7dd80
Mina Doroudi <mina.doroudi@gmail.com>
2012-10-12 21:19:26 -0700

    string matching works for basics at least

--- random/string_in_matrix.rb
+++ random/string_in_matrix.rb
@@ -4,47 +4,46 @@ def string_exist(matrix, word)
   rows = matrix.size
   cols = matrix[0].size
   visited = [Array.new(cols, 0)]*rows
+  rval = false
 
-  for i in (0..rows)
-    for j in (0..cols)
-      if matrix[i][j] = word[0]
-        visited [i][j] = 1
-        do_string_exist(matrix, visited, word[1..word.size], [i,j])
-      end
-    end
-  end
-end
-
-def do_string_exist(matrix, visited, word_rest, curr_ij)
-  rows = matrix.size
-  cols = matrix[0].size
-
-  for for i in (0...rows)
+  for i in (0...rows)
     for j in (0...cols)
-
-      wi = 0
-      while wi < word.size
-        if matrix[i][j] == word[wi]
-          visited[i][j] = 1
-          wi += 1
-          neighs = char_neighbors(matrix, visited, [i,j], word[wi]) 
-          neighs.each do |ng|
-            string_exist(matrix, word)         
-          end
+      if matrix[i][j] == word[0]
+        visited [i][j] = 1
+        rval = do_string_exist(matrix, visited, word[1..word.size], [i,j])
+        unless rval
+          visited =  [Array.new(cols, 0)]*rows
         end
       end
-
     end
   end
+  rval
+end
+
+def do_string_exist(matrix, visited, word, curr_ij)
+  return true if word.size == 0 
+
+  rows = matrix.size
+  cols = matrix[0].size
   
-  return false
+  neighs = char_neighbors(matrix, visited, curr_ij, word[0]) 
+  return false if neighs.size == 0
+
+  neighs.each do |nei|
+    visited[nei[0]][nei[1]] = 1
+    return do_string_exist(matrix, visited, word[1..word.size], nei) 
+  end
 end
 
 
 def char_neighbors(matrix, visited, ij, char)
   res = []
-  for i in (ij[0]-1..ij[0]+1)
-    for j in (ij[1]-1..ij[1]+1)
+  i_range = ([0, ij[0]-2].max .. [matrix.size-1, ij[0]+1].min)
+  j_range = ([0, ij[1]-1].max .. [matrix[0].size-1, ij[1]+1].min)
+  for i in i_range
+    return [] if i >= matrix.size
+    for j in j_range
+      return [] if j >= matrix[0].size
       if matrix[i][j] == char && visited[i][j] != 1
         res << [i,j]
       end
@@ -52,3 +51,20 @@ def char_neighbors(matrix, visited, ij, char)
   end
   res
 end
+
+#alphabet = ("a".."z").to_a
+#matrix = [] 
+#for i in (0...10)
+  #matrix << Array.new(10, "")
+#end
+
+#for i in (0...10)
+  #for j in (0...10)
+    #matrix[i][j] = alphabet[rand(0..25)]
+  #end
+#end
+#puts matrix.to_s
+
+#matrix = [%w(a b c), %w(d e f), %w(m i n)]
+matrix = [%w(a b c)]
+puts string_exist(matrix, "bc")
