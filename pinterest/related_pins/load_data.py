21f4c92127ba224483b8e0a3986ff9c5128f5472
Mina Doroudi <mina.doroudi@gmail.com>
2012-10-23 01:52:40 -0700

    added truncate before loads incase we have already loaded the data

--- pinterest/related_pins/load_data.py
+++ pinterest/related_pins/load_data.py
@@ -13,12 +13,14 @@ MAX_INMEMORY = 10000
 
 # load only boards from file to db
 def load_boards():
+  truncate_before_load('boards')
   boards_file = open('board_json.txt')
   boards = boards_file.readlines()
   insert_all_pins_or_boards(boards, "board")
 
 # load only pins from file to db
 def load_pins():
+  truncate_before_load('pins')
   pins_file = open('pin_json.txt')
   pins = pins_file.readlines()
   insert_all_pins_or_boards(pins, "pin")
@@ -26,12 +28,15 @@ def load_pins():
 # load all pins and baords to db
 def load_all_data():
   load_boards()
-  load_pins()
+  #load_pins()
 
 # save all the values in store to db and empty store
-def store_to_db(store):
+def store_to_db():
   store.commit()
   store.flush()
+
+def truncate_before_load(tb_name):
+  store.execute("truncate "+tb_name)
 
 # helper function for a given set of json data of type pin or board insert them to db
 def insert_all_pins_or_boards(all_data, class_name):
@@ -46,9 +51,9 @@ def insert_all_pins_or_boards(all_data, class_name):
       store.add(Board(data))
 
     if counter == MAX_INMEMORY :
-      store_to_db(store)
+      store_to_db()
       counter = 0
-  store_to_db(store)
+  store_to_db()
 
 
 load_all_data()
