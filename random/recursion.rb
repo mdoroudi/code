e057290d1385f8b7a115eaf06d319d6ed53a7c27
Mina Doroudi <mina.doroudi@gmail.com>
2012-11-14 17:09:21 -0800

    add to zero

--- random/recursion.rb
+++ random/recursion.rb
@@ -42,7 +42,7 @@ def three_combinations(nums)
   res
 end
 
-# if sum of any 3 out of n numbes add to zero
+# if sum of any 3 out of n numbes add to zero, takes O(n^3)
 def three_add_to_zero?(nums)
   for i in (0...nums.size())
     for j in (i+1...nums.size())
@@ -56,9 +56,29 @@ def three_add_to_zero?(nums)
   return false
 end
 
-nums = [0,1,2,3,4,5]
-puts "supposed to have total: #{nums.size()*(nums.size()-1)*(nums.size()-2)/6} for "+nums.to_s
+def three_add_to_zero_faster?(nums)
+  my_hash = {}
+  for i in (0...nums.size())
+    for j in (i+1...nums.size())
+      sum = nums[i]+nums[j]
+      curr_pair = [nums[i], nums[j]]
+      if my_hash[sum].nil?
+        my_hash[sum] = [curr_pair]
+      else
+        my_hash[sum] << curr_pair
+      end
+    end
+  end
+  nums.each do |number|
+    return [number]+my_hash[-number][0] if !my_hash[-number].nil?
+  end
+  return false
+end
+
+nums = [0,1,2,-3,4,5]
+#puts "supposed to have total: #{nums.size()*(nums.size()-1)*(nums.size()-2)/6} for "+nums.to_s
 #three_numbers_add_to_zero(nums)
 #three_combinations(nums)
-n_comb_rec(nums, 3)
-n_comb_rec(nums,1)
+#n_comb_rec(nums, 3)
+#n_comb_rec(nums,1)
+puts three_add_to_zero_faster?(nums).to_s
